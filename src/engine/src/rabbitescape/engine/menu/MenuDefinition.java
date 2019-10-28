76153a404f7d09ec832ab8f87e67d3ffbc968da8
Nurseli ŞAR <nurselisar@Nurseli-MacBook-Air.local>
2019-10-28 14:10:29 +0300

    first

--- src/engine/src/rabbitescape/engine/menu/MenuDefinition.java
+++ src/engine/src/rabbitescape/engine/menu/MenuDefinition.java
@@ -53,7 +53,7 @@ public class MenuDefinition
                 ),
                 true
             ),
-            maybeItem(
+         maybeItem(
             	includeLoadLevel,
             	"Settings",
             	menu(
